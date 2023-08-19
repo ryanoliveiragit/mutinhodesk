e209e8c715d57e83b8f5fb2f1d6b53190c70d6ef
Ryan Oliveira <97217108+ryanoliveiragit@users.noreply.github.com>
2023-08-19 00:46:07 -0300

    feature/login input textfield

--- mutinhosite/src/pages/login/login.tsx
+++ mutinhosite/src/pages/login/login.tsx
@@ -1,23 +1,61 @@
-import './styles.css'
+import "./styles.css";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import TextField from "@mui/material/TextField";
 
+const loginFormSchema = z.object({
+  email: z
+    .string({
+      errorMap: () => {
+        return { message: "Digite um e-mail válido" };
+      },
+    })
+    .email(),
+  password: z.string({
+    errorMap: () => {
+      return { message: "Senha inválida" };
+    },
+  }),
+});
+
+type LoginFormInputs = z.infer<typeof loginFormSchema>;
 export const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<LoginFormInputs>({
+    resolver: zodResolver(loginFormSchema),
+  });
 
-    function submitForm (e: any){
-        e.preventDefault()
-        console.log('enviando dados')
-    }
+  const handleForm = (data: LoginFormInputs) => {
+    console.log(data, "enviando dados");
+    reset();
+  };
 
-    return (
-        <div className='container'>
-            <h1>MutantWhats</h1>
-            <form onSubmit={submitForm}>
-
-                <input type="text" placeholder='E-mail' />
-                <input type="password" placeholder='Senha' />
-
-                <button type='submit'>Login</button>
-            </form>
-        </div>
-        
-    )
-}
+  return (
+    <div className="container">
+      <h1>MutantWhats</h1>
+      <form onSubmit={handleSubmit(handleForm)}>
+        <TextField
+          error={!!errors.email?.message}
+          helperText={errors.email?.message}
+          type="text"
+          {...register("email")}
+          placeholder="E-mail"
+        />
+        <TextField
+        error={!!errors.password?.message}
+        helperText={errors.password?.message}
+          type="password"
+          {...register("password")}
+          placeholder="Senha"
+        />
+        <span>{!!errors.password?.message}</span>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
